c91fcdc1eb43c96f392df89b8420a6cad1a0ab2d
Kalana Indula <kalanasmedia@outlook.com>
2023-11-25 12:50:58 +0530

    second commit

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("Trail code for git");
         System.out.println("Test code 01");
+        System.out.println("test code 02");
     }
 }
